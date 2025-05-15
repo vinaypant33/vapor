725e0e6c8de72173bf1958442163d75515c38f19
Vinay Pant <vinaypant24@gmail.com>
2025-05-15 12:35:21 +0100

    Initial Code for Tokens

--- tokens.py
+++ tokens.py
@@ -1,4 +1,28 @@
 
 
 # Enum Style for all the tokens that would be in the programming language : 
-let_it  = "LET_IT"
+LET_IT  = "LET_IT"
+PRINT = "PRINT"
+IDENTIFIER = "IDENTIFIER"
+EQUAL  = "EQUAL"
+NUMBER  =  "NUMBER"
+PLUS = "PLUS"
+MINUS  = "MINUS"
+LPAREN  = "LPAREN"
+RPAREN  = "RPAREN"
+EOF  = "EOF"
+
+
+# Class to hold the value when the reader is working : 
+class Token():
+
+    def __init__(self , type_ , value  = None):
+        self.type = type_
+        self.value  = value
+
+    
+    def __repr__(self):
+        if self.value : 
+            return f'{self.type} : {self.value}'
+        return f"{self.type}"
+
